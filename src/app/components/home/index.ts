3d7379e81b18b788f09d0ef93a31cedef847b724
Jaakko Maatiala <jaakkooplay@gmail.com>
2026-01-14 12:39:26 +0200

    navbar

--- src/app/components/home/index.ts
+++ src/app/components/home/index.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'homepage-root',
-  templateUrl: 'index.html',
-  styleUrls: ['home.css']
+  templateUrl: './home.html',
+  styleUrls: ['./home.css']
 })
 export class Home {} 
--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -8,10 +8,21 @@ import { Component, HostListener } from "@angular/core";
 export class Navigationbar {
 
     isScrolled = false;
+    isMenuOpen = false;
 
     @HostListener('window:scroll', [])
     onWindowScroll() {
         this.isScrolled = window.scrollY > 100;
+    }
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+        document.body.style.overflow = this.isMenuOpen ? 'hidden' : '';
+    }
+
+    closeMenu() {
+        this.isMenuOpen = false;
+        document.body.style.overflow = '';
     }
 
 }
